af4011768329623e424520597c85b2bc0362f699
yuubd <gc.kim93@hotmail.com>
2018-03-25 00:28:49 -0700

    worked on  put & post

--- src/api/routes/AuthableLoader.js
+++ src/api/routes/AuthableLoader.js
@@ -1,13 +1,13 @@
-//Base Authable Route Loading Class
+// Base Authable Route Loading Class
 
 module.exports =
-class AuthableLoader extends require("./RoutesLoader") {
-	constructor(db, authLoader) {
-		super(db);
-		this.__auth = authLoader;
-	}
-	
-	authenticate(token) {
-		return this.__auth.authenticate(token);
-	}
+class AuthableLoader extends require('./RoutesLoader') {
+  constructor(db, authLoader) {
+    super(db)
+    this.__auth = authLoader
+  }
+
+  authenticate(token) {
+    return this.__auth.authenticate(token)
+  }
 }
