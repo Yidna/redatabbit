9876bfe9962f37976bf16574cbfc3482d5a67467
yuubd <gc.kim93@hotmail.com>
2018-03-26 16:09:09 -0700

    eslint rules changed and SubboardsLoader basic api added

--- src/api/routes/SubboardsLoader.js
+++ src/api/routes/SubboardsLoader.js
@@ -1,25 +1,55 @@
-//Subboards Route
-module.exports = 
-class SubboardsLoader extends require("./AuthableLoader") {
-	loadRoutes(router) {
-		router.get("/subboards", (req, res) => {
-			return this.sendSuccessData(res, [{ "name": "allBoards", "date_created": "howtoformatdate" }]);
-		});
-		
-		router.get("/subboards/:name", (req, res) => {
-			return this.sendSuccessData(res, [{ "name": "singleBoard", "date_created": "howtoformatdate" }]);
-		});
-				
-		router.put("/subboards/:name", (req, res) => {
-			return this.sendSuccess(res);
-		});
-		
-		router.post("/subboards", (req, res) => {
-			return this.sendSuccess(res);
-		});
-		
-		router.delete("/subboards/:name", (req, res) => {
-			return this.sendSuccess(res);
-		});
-	}
-}
+// Subboards Route
+module.exports =
+  class SubboardsLoader extends require('./AuthableLoader') {
+    loadRoutes(router) {
+      router.get('/subboards', (req, res) => {
+        const q = 'SELECT * FROM Subboard'
+        this.db.query(q, (err, rows) => {
+          if (err) {
+            throw err
+          }
+          return this.sendSuccessData(res, rows)
+        })
+      })
+
+      router.get('/subboards/:name', (req, res) => {
+        const q = 'SELECT * FROM Subboard WHERE name=?'
+        this.db.query(q, [req.param.name], (err, rows) => {
+          if (err) {
+            throw err
+          }
+          return this.sendSuccessData(res, rows)
+        })
+      })
+
+      router.put('/subboards/:name', (req, res) => {
+        const q = 'UPDATE name=?, date_created=? FROM Subboard WHERE name=?'
+        this.db.query(q, [req.body.name, req.body.date_created, req.param.name], (err, rows) => {
+          if (err) {
+            throw err
+          }
+        })
+        return this.sendSuccess(res)
+      })
+
+      router.post('/subboards', (req, res) => {
+        const q = 'INSERT INTO Subboard(name, date_created) VALUE (?, ?)'
+        this.db.query(q, [req.body.name, req.body.date_created], (err, rows) =>{
+			    if (err) {
+			  	throw err
+          }
+          return this.sendSuccess(res)
+        })
+      })
+
+      router.delete('/subboards/:name', (req, res) => {
+        const q = 'DELETE FROM Subboard WHERE name=?'
+        this.db.query(q, [req.param.name], (err, rows) => {
+          if (err){
+            throw err
+          }
+          return this.sendSuccess(res)
+        })
+      })
+    }
+  }
