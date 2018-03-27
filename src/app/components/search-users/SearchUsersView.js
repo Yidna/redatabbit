e303b71a1886cf84690ac01e05728c9bdc2c0fc3
Ivan Gill <ivansg44@gmail.com>
2018-03-27 15:59:55 -0700

    Implemented return new set of users when searching users

--- src/app/components/search-users/SearchUsersView.js
+++ src/app/components/search-users/SearchUsersView.js
@@ -3,6 +3,7 @@ import $ from 'jquery'
 
 import SearchUsersModel from './SearchUsersModel'
 import template from './SearchUsersTemplate.hbs'
+import UserCollectionView from "../user-list/UserCollectionView";
 
 export default Marionette.View.extend({
   template,
@@ -14,12 +15,28 @@ export default Marionette.View.extend({
     'click #search-users-submit': 'submitForm'
   },
 
+  initialize() {
+    this.usersCollectionView = new UserCollectionView()
+  },
+
   displayForm() {
     $('.search-users-form').toggle()
   },
 
   submitForm() {
-    const input = $('#create-board-field').val()
-    // TODO: POST query search for users
+    // we will use this value to search for users
+    const input = $('#search-users-field').val()
+
+    // add users satisfying search loop
+    $('.users').remove()
+    this.usersCollectionView.collection.reset()
+    // TODO: search users query
+    $.get('/api/accounts', (data) => {
+      this.usersCollectionView.collection.add(data.data)
+    })
+    this.usersCollectionView.render()
+    $('#content').append(this.usersCollectionView.$el)
+    $('#row0').remove()
+    $('.users').append('<tr id="row0"><td>User name</td><td>Post count</td></tr>')
   }
 })
