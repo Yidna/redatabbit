e57ac92fb1ceadde24738ba37f13c3b6da0a59e6
Ivan Gill <ivansg44@gmail.com>
2018-03-27 18:06:57 -0700

    Delete subboards

--- src/app/components/subboard/SubboardView.js
+++ src/app/components/subboard/SubboardView.js
@@ -1,11 +1,33 @@
+import $ from 'jquery'
 import Marionette from 'backbone.marionette'
 
 import SubboardModel from './SubboardModel'
 import template from './SubboardTemplate.hbs'
+import SubboardCollectionView from "./SubboardCollectionView";
 
 export default Marionette.View.extend({
   template,
   tagName: 'tr',
   className: 'subboard',
-  model: new SubboardModel() // is this necessary?
+  model: new SubboardModel(), // is this necessary?
+
+  initialize() {
+    this.subboardCollectionView = new SubboardCollectionView()
+  },
+
+  events: {
+    'click .delete-subboard': 'deleteSubboard'
+  },
+
+  deleteSubboard() {
+    console.log(this.subboardCollectionView)
+    const name = this.model.get('name')
+    $.ajax({
+      method: 'DELETE',
+      url: `/api/subboards/${name}`,
+      success: (data) => {
+        this.subboardCollectionView.collection.remove(this.model)
+      }
+    })
+  }
 })
