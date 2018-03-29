411bc49b79675475f46abc0931658ebe14490b63
yuubd <gc.kim93@hotmail.com>
2018-03-28 20:39:44 -0700

    specialLoader

--- src/api/routes/SubboardsLoader.js
+++ src/api/routes/SubboardsLoader.js
@@ -7,7 +7,7 @@ module.exports =
         const q = 'SELECT name FROM Subboard'
         this.db.query(q, (err, rows) => {
           if (err) {
-            return this.sendError(res, err)
+            return this.sendError(res, "ERROR! TRY AGAIN!")
           }
           return this.sendSuccessData(res, rows)
         })
@@ -15,11 +15,10 @@ module.exports =
 
       // get the name, moderators, and thread of a subboard
       router.get('/subboards/:name/moderators', (req, res) => {
-        let q = 'SELECT name FROM Subboard WHERE name=?;'
-        q += 'SELECT username From Moderates where Subboard=?;'
+        let q = 'SELECT username From Moderates where Subboard=?;'
         this.db.query(q, [req.params.name, req.params.name], (err, rows) => {
           if (err) {
-            return this.sendError(res, err)
+            return this.sendError(res, "ERROR! TRY AGAIN!")
           }
           return this.sendSuccessData(res, rows)
         })
@@ -29,7 +28,7 @@ module.exports =
         const q = 'UPDATE Subboard SET  name=?, date_created=? WHERE name=?'
         this.db.query(q, [req.body.name, req.body.date_created, req.params.name], (err, rows) => {
           if (err) {
-            return this.sendError(res, err)
+            return this.sendError(res, "ERROR! TRY AGAIN!")
           }
         })
         return this.sendSuccess(res)
@@ -39,15 +38,15 @@ module.exports =
         var q = 'INSERT INTO Subboard (name) VALUES (?);'
         q += 'INSERT INTO Moderates (username, subboard) VALUES (?, ?)'
         if (!req.headers.token){
-          return this.sendError(res, "token does not exist")
+          return this.sendError(res, "Token does not exist")
         }
         var uName = this.authenticate(req.headers.token)
         if (uName == ""){
-          return this.sendError(res, "name does not exist")
+          return this.sendError(res, "Name does not exist")
         }
         this.db.query(q, [req.body.name, uName, req.body.name], (err, rows) =>{
 			    if (err) {
-            return this.sendError(res, err)
+            return this.sendError(res, "ERROR! TRY AGAIN!")
           }
           return this.sendSuccess(res)
         })
@@ -58,7 +57,7 @@ module.exports =
         q += 'DELETE FROM Moderates WHERE username=?'
         this.db.query(q, [req.params.name, req.params.username], (err, rows) => {
           if (err){
-            return this.sendError(res, err)
+            return this.sendError(res, "ERROR! TRY AGAIN!")
           }
           return this.sendSuccess(res)
         })
