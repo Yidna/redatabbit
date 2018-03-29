490ce6558e8df083637f28f320c50235978d3950
Ivan Gill <ivansg44@gmail.com>
2018-03-29 11:21:14 -0700

    Get threads gets post content

--- src/api/routes/ThreadsLoader.js
+++ src/api/routes/ThreadsLoader.js
@@ -3,7 +3,7 @@ module.exports =
     class ThreadsLoader extends require('./AuthableLoader') {
         loadRoutes(router) {
             router.get('/subboards/:subboard_name/threads', (req, res) => {
-				const q = 'SELECT p.id, title, username, date_created FROM Thread t, Post p WHERE subboard=? AND t.id = p.id'
+				const q = 'SELECT p.id, p.content, title, username, date_created FROM Thread t, Post p WHERE subboard=? AND t.id = p.id'
 				this.db.query(q, [req.params.subboard_name], (err, rows) => {
 					if (err) {
 					  return this.sendError(res, err)
@@ -30,7 +30,7 @@ module.exports =
 				if (auth == "") {
 					return this.sendError(res, "No user in token");
 				}
-				
+
 				var q = 'SET @sb := (SELECT DISTINCT subboard FROM Moderates WHERE username=? AND subboard=?);'
 				q += 'SET @id := (SELECT DISTINCT p.id FROM Post p, Thread t WHERE p.id=? AND (p.username=? OR (p.id=t.id AND t.subboard=@sb)));'
 				q += 'UPDATE Post SET content=? WHERE id=@id'
@@ -50,7 +50,7 @@ module.exports =
 				if (auth == "") {
 					return this.sendError(res, "No user in token");
 				}
-				
+
 				var q = 'INSERT INTO Post(username, content) VALUES (?, ?);'
 				q += 'INSERT INTO Thread(id, subboard, title) VALUES (LAST_INSERT_ID(), ?, ?)'
 				this.db.query(
@@ -63,7 +63,7 @@ module.exports =
 					return this.sendSuccess(res)
 				})
             })
-			
+
 			router.delete('/subboards/:subboard_name/threads/:thread_id', (req, res) => {
 				if (!req.headers.token) {
 					return this.sendError(res, "No token");
@@ -72,7 +72,7 @@ module.exports =
 				if (auth == "") {
 					return this.sendError(res, "No user in token");
 				}
-				
+
 				var q = 'SET @sb := (SELECT DISTINCT subboard FROM Moderates WHERE username=? AND subboard=?);'
 				q += 'SET @id := (SELECT DISTINCT p.id FROM Post p, Thread t WHERE p.id=? AND (p.username=? OR (p.id=t.id AND t.subboard=@sb)));'
 				q += 'DELETE FROM Thread WHERE id=@id;'
